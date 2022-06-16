9eeb14ee6a6e2c70c696d6e72831b8c121fd0f6e
HypnoHippo <slechta.forrest@gmail.com>
2022-06-16 00:50:06 -0700

    started transferring site from assignment8, converted router paths to new Nuxt page paths

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,58 @@
 <template>
-  <h1>Hello World!</h1>
+  <!-- Adding in router view and components -->
+  <div id="app">
+    <nav-bar></nav-bar>
+    <HomePage></HomePage>
+    <app-footer></app-footer>
+  </div>
 </template>
 
 <script>
+import HomePage from './HomePage.vue'
+import NavBar from '@/components/NavBar.vue'
+import AppFooter from '@/components/AppFooter.vue'
+
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  components: {
+    NavBar,
+    AppFooter,
+    HomePage
+  }
 }
 </script>
+
+<style lang="scss">
+
+// Giving page not covered by components a dark background
+body {
+  background-color: #343a40;
+}
+
+// Adding dark bg, aligning text, and making text white on all pages
+#app {
+ text-align: center;
+ background-color: #343a40;
+ color: white;
+}
+
+// Styling nav bar and items
+nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+  }
+}
+
+.nav-link {
+  width: 100%;
+  padding: 1rem;
+}
+
+// Making display class text responsive
+@media screen and (max-width: 1200px) {
+  .display-4 {
+    font-size: calc(1.475rem + 2.7vw);
+  }
+}
+</style>
